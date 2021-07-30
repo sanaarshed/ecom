756eeebb1723528f919b1d5a8fc83bed684b10d6
Sana Arshsed <31148211+sanaarshed@users.noreply.github.com>
2021-07-30 00:49:04 -0700

    Navigation Added

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack'
+import {NavigationContainer} from '@react-navigation/native'
+
+
+import AccountScreen from './app/screens/AccountScreen';
+import DisplayImage from './app/screens/DisplayImage';
+import ListingDetails from './app/screens/ListingDetails';
+import Listings from './app/screens/Listings';
+import MessagesScreen from './app/screens/MessagesScreen';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import Login from './app/screens/Login';
 
 export default function App() {
+
+  const Tabs = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   <NavigationContainer>
+     <Tabs.Navigator  
+   initialRouteName ="WelcomeScreen">
+       <Tabs.Screen options={{headerShown: false}} name={"WelcomeScreen"} component={WelcomeScreen}/>
+        <Tabs.Screen name="MessagesScreen" component={MessagesScreen}/>
+       <Tabs.Screen name="AccountScreen" component={AccountScreen}/>
+       <Tabs.Screen name="DisplayImage" component={DisplayImage}/>
+      
+      <Tabs.Screen name="ListingDetails" component={ListingDetails}/>
+      <Tabs.Screen 
+      name="Listings" component={Listings}/>
+      <Tabs.Screen name="Login" component={Login}/> 
+       </Tabs.Navigator>
+     </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
